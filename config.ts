7f57e226cbb596f24a64fda5d75b62a1f14b5e8a
Perry Fardella <perry.fardella@live.com>
2025-08-20 14:12:12 +0300

    Add appName to config file and begin work on supabase email templates.

--- config.ts
+++ config.ts
@@ -1,5 +1,8 @@
 import { Provider } from "@supabase/supabase-js";
 
+// Set the name of your app here
+export const appName = "My App";
+
 // Edit this list to add or remove social auth providers from sign-in and sign-up pages
 // Check the Supabase Provider type for all possible values
 export const socialAuthProviders: Provider[] = [
