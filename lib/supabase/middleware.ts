a5574e90273c64df6ad78f60aa90924d3eac7d81
Perry Fardella <perry.fardella@live.com>
2025-08-21 15:01:32 +0300

    Update middleware to allow non-logged in users to access all pages except the protected dashboard page.

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -40,11 +40,7 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/sign-in") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/sign-in";
